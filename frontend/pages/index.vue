554c62c2c6ee8238dd0359fda17426f2442a432f
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2020-11-08 13:32:56 +0900

    axios入れられたああああああああああああ package.json怖い

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,73 +1,19 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        frontend
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <div>あ</div>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $axios }) {
+    // 取得先のURL
+    const url = "https://qiita.com/api/v2/items";
+    // リクエスト（Get）
+    const response = await $axios.$get(url);
+    console.log(response);
+    // 配列で返ってくるのでJSONにして返却
+    return {
+      posts: response
+    };
+  }
+};
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
