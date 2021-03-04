542581d1b9a9c1225e821ff90e9c4f6ff051e2b6
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2021-03-04 10:57:55 +0900

    pagination fix

--- backend/app/controllers/api/posts_controller.rb
+++ backend/app/controllers/api/posts_controller.rb
@@ -1,39 +1,16 @@
 class Api::PostsController < ApplicationController
-  respond_to :posts
-
   before_action :authenticate_api_user!, only: %i[create show update]
   before_action :set_post, only: %i[show update destroy]
 
   def index
     # posts = cache_posts_index # NOTE: redisのキャッシュ。でも使うの微妙
-    # posts = Post.includes(likes: :user).id_desc
     posts = Post.includes(likes: :user).page(params[:page]).id_desc
-    pagination = resources_with_pagination(posts)
-
-    # respond_with posts, meta: {
-    #   current_page: posts.current_page,
-    #   next_page: posts.next_page,
-    #   prev_page: posts.prev_page,
-    #   total_pages: posts.total_pages,
-    #   total_count: posts.total_count
-    # }
-    # respond_with posts, serializer: PaginationSerializer
-    # render json: { posts: posts, pagination: pagination }, status: :ok
-    # render json: { posts: posts, pagination: pagination }, include: [likes: :user], status: :ok
-
-    # TypeError (no implicit conversion of Symbol into Integer):
-    # render json: { posts: posts, pagination: pagination }, include: [likes: :user], each_serializer: PaginationSerializer, status: :ok
-    # render json: { posts: posts, pagination: pagination }, each_serializer: PaginationSerializer, status: :ok
-    # render json: posts, include: [likes: :user], serializer: PaginationSerializer, status: :ok
-    # render json: posts, include: [likes: :user], status: :ok
-    # render json: posts, include: [likes: :user], status: :ok
-
-    # serializerが反応しない
-    # render json: posts, include: [likes: :user], meta: resources_with_pagination(posts), status: :ok
-    # render json: posts, meta: resources_with_pagination(posts), status: :ok
-
     # adapter: :jsonつけると何故かうまくいく。理由はよく分からん。includeでネストするやつも明示できる！
     render json: posts, meta: resources_with_pagination(posts), include: [likes: :user], status: :ok, adapter: :json
+
+    # adapter: :jsonがないとserializerが反応しない
+    # render json: posts, include: [likes: :user], meta: resources_with_pagination(posts), status: :ok
+
     # rootで指定するとpostsの名前が指定したhogehogeになる
     # render json: posts, root: "hogehoge", meta: resources_with_pagination(posts), include: [likes: :user], status: :ok, adapter: :json
 
