1445799d26a7e0bef73da7bab8957cc786af7f8b
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2021-03-06 16:20:27 +0900

    everyday_rails 3章まで終わり

---
+++ backend/db/migrate/20210306061923_add_column_user_id_to_posts.rb
@@ -0,0 +1,5 @@
+class AddColumnUserIdToPosts < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :posts, :user
+  end
+end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_28_070002) do
+ActiveRecord::Schema.define(version: 2021_03_06_061923) do
 
-  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "post_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -21,21 +21,21 @@ ActiveRecord::Schema.define(version: 2021_02_28_070002) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
-  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id"
     t.datetime "deleted_at"
     t.string "video"
     t.string "sub_name"
     t.boolean "is_special", default: false
     t.integer "likes_count", default: 0, null: false
     t.string "image"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
     t.string "encrypted_password", default: "", null: false
