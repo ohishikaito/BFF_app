b053c1435b31a9a258ed4f8a57d332d14462f1f3
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2021-08-21 13:23:55 +0900

    add min_length

--- backend/spec/factories/user.rb
+++ backend/spec/factories/user.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     sequence(:email) { |n| "#{n}_" + Faker::Internet.email }
     first_name       { Faker::Name.first_name }
     last_name        { Faker::Name.last_name }
-    password         { Faker::Internet.password(8) }
+    password         { Faker::Internet.password(min_length: 8) }
   end
 end
